63c4f7c1b0494676aca78ee7c660efad7ca4ed68
0Camus0 <camus_mm@hotmail.com>
2015-09-28 13:57:45 -0700

    +Added callbacks and future structure.

--- Camus_Project/Libs/Framework/include/WindowManager/AndroidApp.h
+++ Camus_Project/Libs/Framework/include/WindowManager/AndroidApp.h
@@ -1,5 +1,42 @@
 #ifndef CA_ANDROID_APP_H
 #define CA_ANDROID_APP_H
 
+#include <Core/Core.h>
+#include <Driver/BaseDriver.h>
+#include <memory>
+
+#include <android/configuration.h>
+#include <android/looper.h>
+#include <android/native_activity.h>
+
+class AndroidApp : public RootApp {
+public:
+	AndroidApp() :
+		RootApp(true) {}
+	void InitGlobalVars();
+	void OnCreateApplication();
+	void OnDestroyApplication();
+	void OnInterruptApplication();
+	void OnResumeApplication();
+	void UpdateApplication();
+
+	static void onDestroy(ANativeActivity* activity);
+	static void onStart(ANativeActivity* activity);
+	static void onResume(ANativeActivity* activity);
+	static void onSaveInstanceState(ANativeActivity* activity);
+	static void onPause(ANativeActivity* activity);
+	static void onStop(ANativeActivity* activity);
+	static void onConfigurationChanged(ANativeActivity* activity);
+	static void onLowMemory(ANativeActivity* activity);
+	static void onWindowFocusChanged(ANativeActivity* activity);
+	static void onNativeWindowCreated(ANativeActivity* activity);
+	static void onNativeWindowDestroyed(ANativeActivity* activity);
+	static void onInputQueueCreated(ANativeActivity* activity);
+	static void onInputQueueDestroyed(ANativeActivity* activity);
+
+
+	std::unique_ptr<BaseDriver>	m_pVideoDriver;
+};
+
 
 #endif
--- Camus_Project/Libs/Framework/src/WindowManager/AndroidApp.cpp
+++ Camus_Project/Libs/Framework/src/WindowManager/AndroidApp.cpp
@@ -1,1 +1,79 @@
 #include <WindowManager/AndroidApp.h>
+#include <Utils/WindowProperties.h>
+#include <Driver/OpenGLDriver.h>
+
+void AndroidApp::InitGlobalVars() {
+
+}
+
+void AndroidApp::OnCreateApplication() {
+
+}
+
+void AndroidApp::OnDestroyApplication() {
+
+}
+
+void AndroidApp::OnInterruptApplication() {
+
+}
+
+void AndroidApp::OnResumeApplication() {
+
+}
+
+void AndroidApp::UpdateApplication() {
+
+}
+
+void AndroidApp::onDestroy(ANativeActivity* activity){
+
+}
+
+void AndroidApp::onStart(ANativeActivity* activity){
+
+}
+
+void AndroidApp::onResume(ANativeActivity* activity){
+
+}
+
+void AndroidApp::onSaveInstanceState(ANativeActivity* activity){
+
+}
+
+void AndroidApp::onPause(ANativeActivity* activity){
+
+}
+
+void AndroidApp::onStop(ANativeActivity* activity){
+
+}
+
+void AndroidApp::onConfigurationChanged(ANativeActivity* activity){
+
+}
+
+void AndroidApp::onLowMemory(ANativeActivity* activity){
+
+}
+
+void AndroidApp::onWindowFocusChanged(ANativeActivity* activity){
+
+}
+
+void AndroidApp::onNativeWindowCreated(ANativeActivity* activity){
+
+}
+
+void AndroidApp::onNativeWindowDestroyed(ANativeActivity* activity){
+
+}
+
+void AndroidApp::onInputQueueCreated(ANativeActivity* activity){
+
+}
+
+void AndroidApp::onInputQueueDestroyed(ANativeActivity* activity){
+
+}
