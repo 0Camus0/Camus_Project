153ad0e29d4df43d7e68f2563ae8b5010c800049
Camus <camus_mm@hotmail.com>
2015-10-20 02:52:30 -0700

    - Changes

--- Camus_Project/Libs/Framework/include/Driver/Technique.h
+++ Camus_Project/Libs/Framework/include/Driver/Technique.h
@@ -2,6 +2,7 @@
 #define CA_TECHNIQUE_H
 
 #include <Driver/RenderState.h>
+#include <Utils/Maths.h>
 #include <string>
 #include <cstddef>
 
@@ -22,6 +23,15 @@ namespace hyperspace {
 			virtual void			AddPass(Pass_ pass) = 0;
 			virtual void			RemovePass(int id) = 0;
 			virtual std::int32_t	GetNumPasses() = 0;
+
+			virtual	void	SetBool(std::string handler, bool &) = 0;
+			virtual	void	SetInt(std::string handler, int &) = 0;
+			virtual	void	SetFloat(std::string handler, float &) = 0;
+			virtual	void	SetVec2(std::string handler, XVECTOR2 &) = 0;
+			virtual	void	SetVec3(std::string handler, XVECTOR3 &) = 0;
+			virtual	void	SetMat2(std::string handler, float*) = 0;
+			virtual	void	SetMat3(std::string handler, float*) = 0;
+			virtual	void	SetMat4(std::string handler, XMATRIX44 &) = 0;
 
 		private:
 			std::string		Name;
