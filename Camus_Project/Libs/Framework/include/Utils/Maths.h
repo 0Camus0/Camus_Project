b48e140d6d4a3bd2fc7ac565cfa0b3ab65b88ded
0Camus0 <camus_mm@hotmail.com>
2015-10-27 13:57:35 -0700

    -Cleaning the library.

--- Camus_Project/Libs/Framework/include/Utils/Maths.h
+++ Camus_Project/Libs/Framework/include/Utils/Maths.h
@@ -305,42 +305,6 @@ namespace hyperspace {
 			XQUATERNION(const XVECTOR3& v) :x(v.x), y(v.y), z(v.z), w(v.w) {}
 			XQUATERNION(const XQUATERNION& v) :x(v.x), y(v.y), z(v.z), w(v.w) {}
 			XQUATERNION(const float &xp, const float &yp, const float &zp, const float &wp) :x(xp), y(yp), z(zp), w(wp) {}
-
-			/*XVECTOR3 & operator= (const XVECTOR3 & other){
-				this->x = other.x; this->y = other.y; this->z = other.z; this->w = other.w;
-				return *this;
-			}
-			XVECTOR3 & operator= (XVECTOR3 & other){
-				this->x = other.x; this->y = other.y; this->z = other.z; this->w = other.w;
-				return *this;
-			}
-
-			operator float* ();
-			operator const float* () const;
-
-			XVECTOR3& operator += ( const XVECTOR3& );
-			XVECTOR3& operator -= ( const XVECTOR3& );
-			XVECTOR3& operator *= ( float );
-			XVECTOR3& operator /= ( float );
-
-			XVECTOR3 operator + () const;
-			XVECTOR3 operator - () const;
-
-			XVECTOR3 operator + ( const XVECTOR3& ) const;
-			XVECTOR3 operator - ( const XVECTOR3& ) const;
-			XVECTOR3 operator * ( float ) const;
-			XVECTOR3 operator / ( float ) const;
-
-			float	   operator * ( const XVECTOR3& );
-
-			friend XVECTOR3 operator * ( float, const struct XVECTOR3& );
-
-			bool operator == ( const XVECTOR3& ) const;
-			bool operator != ( const XVECTOR3& ) const;
-
-			void Normalize();
-			float Length();
-		*/
 			union {
 				float	 v[4];
 
@@ -352,9 +316,8 @@ namespace hyperspace {
 				};
 			};
 		};
-#include <stdio.h>
-		inline void XMatMultiply(XMATRIX44 &mpout, const XMATRIX44 &mp1, const XMATRIX44 &mp2) {
 
+		inline void XMatMultiply(XMATRIX44 &mpout, const XMATRIX44 &mp1, const XMATRIX44 &mp2) {
 			
 			float a00 = mp1.m[0][0], a01 = mp1.m[0][1], a02 = mp1.m[0][2], a03 = mp1.m[0][3],
 				a10 = mp1.m[1][0], a11 = mp1.m[1][1], a12 = mp1.m[1][2], a13 = mp1.m[1][3],
@@ -382,47 +345,10 @@ namespace hyperspace {
 			mpout.m[3][1] = a30*b01 + a31*b11 + a32*b21 + a33*b31;
 			mpout.m[3][2] = a30*b02 + a31*b12 + a32*b22 + a33*b32;
 			mpout.m[3][3] = a30*b03 + a31*b13 + a32*b23 + a33*b33;
-			/*
-				float a00 = mp1.m[0][0], a01 = mp1.m[0][1], a02 = mp1.m[0][2], a03 = mp1.m[0][3],
-					a10 = mp1.m[1][0], a11 = mp1.m[1][1], a12 = mp1.m[1][2], a13 = mp1.m[1][3],
-					a20 = mp1.m[2][0], a21 = mp1.m[2][1], a22 = mp1.m[2][2], a23 = mp1.m[2][3],
-					a30 = mp1.m[3][0], a31 = mp1.m[3][1], a32 = mp1.m[3][2], a33 = mp1.m[3][3];
-
-				float b00 = mp2.m[0][0], b01 = mp2.m[0][1], b02 = mp2.m[0][2], b03 = mp2.m[0][3],
-					b10 = mp2.m[1][0], b11 = mp2.m[1][1], b12 = mp2.m[1][2], b13 = mp2.m[1][3],
-					b20 = mp2.m[2][0], b21 = mp2.m[2][1], b22 = mp2.m[2][2], b23 = mp2.m[2][3],
-					b30 = mp2.m[3][0], b31 = mp2.m[3][1], b32 = mp2.m[3][2], b33 = mp2.m[3][3];
-
-				mpout.m[0][0]  = -1.0f*(a00*b02 + a01*b12 + a02*b22 + a03*b32);
-				mpout.m[0][1]  = a00*b01 + a01*b11 + a02*b21 + a03*b31;
-				mpout.m[0][2]  = a00*b00 + a01*b10 + a02*b20 + a03*b30;
-				mpout.m[0][3]  = a00*b03 + a01*b13 + a02*b23 + a03*b33;
-
-				mpout.m[1][0]  = -1.0f*(a10*b02 + a11*b12 + a12*b22 + a13*b32);
-				mpout.m[1][1]  = a10*b01 + a11*b11 + a12*b21 + a13*b31;
-				mpout.m[1][2]  = a10*b00 + a11*b10 + a12*b20 + a13*b30;
-				mpout.m[1][3]  = a10*b03 + a11*b13 + a12*b23 + a13*b33;
-
-				mpout.m[2][0]  = -1.0f*( a20*b02 + a21*b12 + a22*b22 + a23*b32);
-				mpout.m[2][1]  = a20*b01 + a21*b11 + a22*b21 + a23*b31;
-				mpout.m[2][2]  = a20*b00 + a21*b10 + a22*b20 + a23*b30;
-				mpout.m[2][3]  = a20*b03 + a21*b13 + a22*b23 + a23*b33;
-
-				mpout.m[3][0]  = -1.0f*(a30*b02 + a31*b12 + a32*b22 + a33*b32);
-				mpout.m[3][1]  = a30*b01 + a31*b11 + a32*b21 + a33*b31;
-				mpout.m[3][2]  = a30*b00 + a31*b10 + a32*b20 + a33*b30;
-				mpout.m[3][3]  = a30*b03 + a31*b13 + a32*b23 + a33*b33;
-			*/
-
-
-
-
-
 		}
 
 		inline void XMatTranslation(XMATRIX44 &mpout, const float &x, const float &y, const float &z) {
 			XMatIdentity(mpout);
-			//mpout.m[0][3] = x; mpout.m[1][3] = y; mpout.m[2][3] = z; mpout.m[3][3] = 1.0f;
 			mpout.m[3][0] = x; mpout.m[3][1] = y; mpout.m[3][2] = z; mpout.m[3][3] = 1.0f;
 		}
 
