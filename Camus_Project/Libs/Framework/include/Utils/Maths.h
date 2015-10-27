4db867056cc8d14c9403d23cf93cdce7ec4cefa3
0Camus0 <camus_mm@hotmail.com>
2015-10-27 14:22:31 -0700

    - More cleaning.

--- Camus_Project/Libs/Framework/include/Utils/Maths.h
+++ Camus_Project/Libs/Framework/include/Utils/Maths.h
@@ -915,7 +915,34 @@ namespace hyperspace {
 		inline XMATRIX44 XMATRIX44::operator * (const XMATRIX44& mb) const
 		{
 			XMATRIX44	tmp;
-			XMatMultiply(tmp, *this, mb);
+
+			float a00 = this->m[0][0], a01 = this->m[0][1], a02 = this->m[0][2], a03 = this->m[0][3],
+				a10 = this->m[1][0], a11 = this->m[1][1], a12 = this->m[1][2], a13 = this->m[1][3],
+				a20 = this->m[2][0], a21 = this->m[2][1], a22 = this->m[2][2], a23 = this->m[2][3],
+				a30 = this->m[3][0], a31 = this->m[3][1], a32 = this->m[3][2], a33 = this->m[3][3];
+
+			float b00 = mb.m[0][0], b01 = mb.m[0][1], b02 = mb.m[0][2], b03 = mb.m[0][3],
+				b10 = mb.m[1][0], b11 = mb.m[1][1], b12 = mb.m[1][2], b13 = mb.m[1][3],
+				b20 = mb.m[2][0], b21 = mb.m[2][1], b22 = mb.m[2][2], b23 = mb.m[2][3],
+				b30 = mb.m[3][0], b31 = mb.m[3][1], b32 = mb.m[3][2], b33 = mb.m[3][3];
+
+			tmp.m[0][0] = a00*b00 + a01*b10 + a02*b20 + a03*b30;
+			tmp.m[0][1] = a00*b01 + a01*b11 + a02*b21 + a03*b31;
+			tmp.m[0][2] = a00*b02 + a01*b12 + a02*b22 + a03*b32;
+			tmp.m[0][3] = a00*b03 + a01*b13 + a02*b23 + a03*b33;
+			tmp.m[1][0] = a10*b00 + a11*b10 + a12*b20 + a13*b30;
+			tmp.m[1][1] = a10*b01 + a11*b11 + a12*b21 + a13*b31;
+			tmp.m[1][2] = a10*b02 + a11*b12 + a12*b22 + a13*b32;
+			tmp.m[1][3] = a10*b03 + a11*b13 + a12*b23 + a13*b33;
+			tmp.m[2][0] = a20*b00 + a21*b10 + a22*b20 + a23*b30;
+			tmp.m[2][1] = a20*b01 + a21*b11 + a22*b21 + a23*b31;
+			tmp.m[2][2] = a20*b02 + a21*b12 + a22*b22 + a23*b32;
+			tmp.m[2][3] = a20*b03 + a21*b13 + a22*b23 + a23*b33;
+			tmp.m[3][0] = a30*b00 + a31*b10 + a32*b20 + a33*b30;
+			tmp.m[3][1] = a30*b01 + a31*b11 + a32*b21 + a33*b31;
+			tmp.m[3][2] = a30*b02 + a31*b12 + a32*b22 + a33*b32;
+			tmp.m[3][3] = a30*b03 + a31*b13 + a32*b23 + a33*b33;
+
 			return tmp;
 		}
 
